e78ce5613991ce503b0481b50e5b1a980c8ae866
neha1137 <86520366+neha1137@users.noreply.github.com>
2022-12-14 18:58:10 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+ import * as firebase from "firebase";
+
+//initialize your database
+const firebaseConfig = {
+  apiKey: "AIzaSyA-_0cHBqZH6jHiPjNwlrwi8QH5typAveQ",
+  authDomain: "attendance-88076.firebaseapp.com",
+  projectId: "attendance-88076",
+  storageBucket: "attendance-88076.appspot.com",
+  messagingSenderId: "850436965869",
+  appId: "1:850436965869:web:65352663bea25e32be53e4"
+};
+
+  export default firebase.database
+ 
+
+  
